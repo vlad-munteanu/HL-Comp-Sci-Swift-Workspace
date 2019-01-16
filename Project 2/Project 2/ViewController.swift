ce9c30f6706d563a82dc9ef9a873f6c5768a9abe
Vlad Munteanu <vladcmunteanu@gmail.com>
2019-01-16 17:19:47 -0500

    first commit

---
+++ Project 2/Project 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Vlad Munteanu on 1/16/19.
+//  Copyright © 2019 LesGarcons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
