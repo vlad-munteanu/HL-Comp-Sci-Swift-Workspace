3312e662eedc4b316219ff6b89204137a63bc823
vlad-munteanu <vladcmunteanu@gmail.com>
2019-02-06 09:04:44 -0500

    Fixed Folder Structure

--- DrunkCube/DrunkCube/Scenes/MainMenuScene.swift
+++ DrunkCube/DrunkCube/Scenes/MainMenuScene.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2019 Vlad Munteanu. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class MainMenuScene : SKScene  {
+
+    let background = SKSpriteNode(imageNamed: "bg")
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        
+        let touch:UITouch = touches.first! as UITouch
+        let positionInScene = touch.location(in: self)
+        let touchedNode = self.atPoint(positionInScene)
+        if let name = touchedNode.name {
+        }
+    }
+}
