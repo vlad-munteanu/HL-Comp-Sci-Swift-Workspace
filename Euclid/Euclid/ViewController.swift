db3c6c2e26116e36e936a03f58f98b374a90ba4f
vlad-munteanu <vladcmunteanu@gmail.com>
2019-02-12 08:28:07 -0500

    created Euclid program

---
+++ Euclid/Euclid/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Euclid
+//
+//  Created by Vlad Munteanu on 2/12/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var outputLet: UILabel!
+    
+    @IBOutlet weak var numberOneField: UITextField!
+    @IBOutlet weak var numberTwoField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    @IBAction func findGCDbutton(_ sender: Any) {
+        if(numberOneField != nil) && (numberTwoField != nil) {
+            
+        }
+    }
+}
+
