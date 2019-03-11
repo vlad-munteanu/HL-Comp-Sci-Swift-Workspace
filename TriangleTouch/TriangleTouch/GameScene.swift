443f0c9182aab99ab61306026c0763f42dfdfbbc
vlad-munteanu <vladcmunteanu@gmail.com>
2019-03-11 08:54:14 -0400

    Fixed triangle

--- TriangleTouch/TriangleTouch/GameScene.swift
+++ TriangleTouch/TriangleTouch/GameScene.swift
@@ -13,52 +13,34 @@ class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
-    var touchePos: [CGPoint]()
+    var touchePos = [CGPoint]()
     
     override func didMove(to view: SKView) {
+
         
-//        // Get label node from scene and store it for use later
-//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-//        if let label = self.label {
-//            label.alpha = 0.0
-//            label.run(SKAction.fadeIn(withDuration: 2.0))
-//        }
-//
-//        // Create shape node to use during mouse interaction
-//        let w = (self.size.width + self.size.height) * 0.05
-//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-//
-//        if let spinnyNode = self.spinnyNode {
-//            spinnyNode.lineWidth = 2.5
-//
-//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-//                                              SKAction.fadeOut(withDuration: 0.5),
-//                                              SKAction.removeFromParent()]))
-//        }
-        
-        self.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        self.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if(touchePos.count <= 2 ) {
+        if(touchePos.count <= 1 ) {
             touchePos.append(pos)
-        } else if(touchePos.count >= 3 ) {
+            let point = SKShapeNode(circleOfRadius: 5.0)
+            point.fillColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+            point.position = pos
+            self.addChild(point)
+        } else if(touchePos.count >= 2) {
             touchePos.append(pos)
+             self.removeAllChildren()
+            
             makeTriangle()
         }
     }
     
     func makeTriangle() {
-        var points = [CGPoint(touchePos[0]),
-                      CGPoint(touchePos[1]),
-                      CGPoint(touchePos[2])]
+        let Triangle = SKShapeNode(points: &touchePos, count: touchePos.count)
         
-        let Triangle = SKShapeNode(points: &points, count: points.count)
-        
-        // 塗りつぶしの色を赤色に指定.
-        Triangle.fillColor = UIColor.red
+        Triangle.fillColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
          self.addChild(Triangle)
         touchePos.removeAll()
 
@@ -68,10 +50,10 @@ class GameScene: SKScene {
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchesMoved(toPoint: t.location(in: self)) }
+//    }
+//
     
     
     override func update(_ currentTime: TimeInterval) {
