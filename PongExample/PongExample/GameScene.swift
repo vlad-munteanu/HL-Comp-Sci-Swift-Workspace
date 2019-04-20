f7c976d657eea5f020ebdf7cbe7eb09092159328
vlad-munteanu <vladcmunteanu@gmail.com>
2019-04-20 16:46:57 -0400

    added ball movement

--- PongExample/PongExample/GameScene.swift
+++ PongExample/PongExample/GameScene.swift
@@ -10,101 +10,65 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+  
+    let ball = SKShapeNode(circleOfRadius: 20.0)
+    var isBallGoingRight = true
+    var isBallGoingUp = true
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var xSpeed: CGFloat = 10.0
+    var ySpeed: CGFloat = 10.0
     
     override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+        backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        ball.fillColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        addChild(ball)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+      
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        // this code is for making ball go right/left
+        if(ball.position.x >= 273) {
+            isBallGoingRight = false
         }
         
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
+        if isBallGoingRight == true {
+            ball.position.x += xSpeed
+        } else {
+            if(ball.position.x <= -273) {
+                isBallGoingRight = true
+            }
+             ball.position.x -= xSpeed
         }
         
-        self.lastUpdateTime = currentTime
+
+        //print(ball.position.x)
+        
+        
+        // this code is for making ball go up/down
+        if(ball.position.y >= 600) {
+            isBallGoingUp = false
+        }
+        
+        if isBallGoingUp == true {
+            ball.position.y += ySpeed
+        } else {
+            if(ball.position.y <= -600) {
+                isBallGoingUp = true
+            }
+            ball.position.y -= ySpeed
+        }
+        
+        print(ball.position.y)
+        
     }
 }
