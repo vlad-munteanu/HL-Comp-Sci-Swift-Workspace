bf563c6d9bea72b9566c15fb88bc707b97e7079f
vlad-munteanu <vladcmunteanu@gmail.com>
2019-04-23 08:37:04 -0400

    added Collatz problem

--- MathStuff/MathStuff/ViewController.swift
+++ MathStuff/MathStuff/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var numberLabel: UILabel!
     
+    var currentNumber = (number: 0,count: 1)
+    var tempCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,6 +22,41 @@ class ViewController: UIViewController {
     
     
     @IBAction func calculateButton(_ sender: Any) {
+        for i in 0..<1000000 {
+           // print(i)
+            checkNum(num: i)
+            if(tempCount > currentNumber.count) {
+                currentNumber.number = i
+                currentNumber.count = tempCount
+            }
+            tempCount = 0
+        }
+        tempCount = currentNumber.count
+        print(tempCount)
+        print(currentNumber.number)
+        numberLabel.text = "Number:\(currentNumber.number)"
+//        print(currentNumber.count)
+    }
+    
+    func checkNum(num: Int) -> Int{
+        
+        if num > 1 {
+            //even
+            if num % 2 == 0 {
+                tempCount += 1
+                //print(tempCount)
+                return checkNum(num: num/2)
+            }
+                //odd
+            else {
+                
+                tempCount += 1
+                return checkNum(num: (3*num) + 1)
+                
+            }
+        } else  {
+            return 0
+        }
         
     }
     
