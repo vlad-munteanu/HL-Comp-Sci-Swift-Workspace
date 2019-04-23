ee40a091969b80cc434f614abb33e0400f2add57
vlad-munteanu <vladcmunteanu@gmail.com>
2019-04-23 08:15:16 -0400

    added interface

--- MathStuff/MathStuff/ViewController.swift
+++ MathStuff/MathStuff/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        
+    }
+    
 
 }
 
