9b200f68cf3e35f580ae0cce4dc7b5112313749d
vlad-munteanu <vladcmunteanu@gmail.com>
2019-05-19 23:15:15 -0400

    added game classes

---
+++ BlackJack/BlackJack/Model/Card.swift
@@ -0,0 +1,12 @@
+//
+//  Card.swift
+//  BlackJack
+//
+//  Created by Vlad Munteanu on 5/19/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import Foundation
+struct Card {
+    var rank: Int
+}
---
+++ BlackJack/BlackJack/Model/Game.swift
@@ -0,0 +1,22 @@
+//
+//  Game.swift
+//  BlackJack
+//
+//  Created by Vlad Munteanu on 5/19/19.
+//  Copyright © 2019 Vlad Munteanu. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+    //var winner: String
+    
+    func checkTwentyOne() {
+        
+    }
+    
+    init() {
+        
+    }
+}
